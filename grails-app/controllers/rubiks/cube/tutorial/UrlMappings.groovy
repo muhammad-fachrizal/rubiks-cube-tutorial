f71533c8fd75a6a15a5c8d0376616b2bca9b7491
Muhammad Fachrizal <mfachrizal17@gmail.com>
2021-12-14 23:46:19 +0700

    Update UrlMappings

--- grails-app/controllers/rubiks/cube/tutorial/UrlMappings.groovy
+++ grails-app/controllers/rubiks/cube/tutorial/UrlMappings.groovy
@@ -3,16 +3,21 @@ package rubiks.cube.tutorial
 class UrlMappings {
 
     static mappings = {
-        "/$controller/$action?/$id?(.$format)?"{
-            constraints {
-                // apply constraints here
-            }
-        }
-
         "/"(controller: "homepage", action: "index")
-        "/cms/login"(controller: 'login', action: 'auth')
-        "/cms/index"(controller: 'articleManagement', action: 'index')
         "500"(view:'/error')
         "404"(view:'/notFound')
+
+        "/homepage"(controller: "homepage", action: "index")
+        "/article"(controller: "article", action: "index")
+
+        "/login/auth"(controller: 'login', action: 'auth')
+        "/logout/index"(controller: "logout", action: "index")
+
+        group "/cms", {
+            "/login"(controller: "login", action: "auth")
+            "/"(controller: "articleManagement", action: "index")
+            "/articleManagement"(controller: "articleManagement", action: "index")
+            "/articleManagement/$action?/$id?"(controller: "articleManagement")
+        }
     }
 }
